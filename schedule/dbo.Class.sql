241030bb770bf351e838f35209177efd77ceba0d
Brandon <bpcollie@geneva.edu>
2014-04-28 12:50:50 -0400

    Database stuff

---
+++ schedule/dbo.Class.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[ClassId] INT NOT NULL PRIMARY KEY, 
+    [Dept] VARCHAR(10) NULL, 
+    [ClassNum] INT NULL, 
+    [Days] VARCHAR(10) NULL, 
+    [Time] VARCHAR(10) NULL, 
+    [Semester] VARCHAR(10) NULL, 
+    [SectionNum] VARCHAR(10) NULL
+)
